8edf383b2f0edcbbb70e857ad6688ae7bbbcaf57
C. Leow <chienleow1@gmail.com>
2020-04-15 14:05:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,6 +2,7 @@ require 'nokogiri'
 require 'open-uri'
  
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+<<<<<<< HEAD
 doc.css(".headline-26OIBN")
 
 courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
@@ -9,3 +10,8 @@ courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo00
 courses.each do |course|
   puts course.text.strip
 end
+=======
+headline = doc.css(".headline-26OIBN").text
+
+puts headline
+>>>>>>> cc3f1917e7d76a37e4d536af80ff8d5739752dd7
